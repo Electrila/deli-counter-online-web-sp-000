de142da259300973b33d6b319011e7885f3d5e79
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 14:59:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  line = []
+  line = ""
   if katz_deli.length == 0
-    line << name
+    line << "Welcome, #{name}. You are number 1 in line."
   #else
     #katz_deli << "#{name}"
   end
