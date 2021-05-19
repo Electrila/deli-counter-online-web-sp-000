ab82fe0c4165b7cad41e977e027c3d0b54f8ef77
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 14:58:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  line = ""
+  if katz_deli.length == 0
+    line << "The line is currently empty."
+  else
+    line << "The line is currently: "
+    last_person = katz_deli[-1]
+    katz_deli.delete_at(-1)
+    katz_deli.each_with_index do |name, index|
+      line << "#{index + 1}. #{name} "
+    end
+    line << "#{katz_deli.length + 1}. #{last_person}"
+  end
+  puts line
+end
+
+def take_a_number(katz_deli, name)
+  line = []
+  if katz_deli.length == 0
+    line << name
+  #else
+    #katz_deli << "#{name}"
+  end
+  puts line
+end
